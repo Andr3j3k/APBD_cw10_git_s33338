923ed286f68bd213c3b306c8be9f3efbd1f83364
Andrzej Pawlak <s33338@pjwstk.edu.pl>
2026-05-12 18:23:52 +0200

    Zrobiono AppData

---
+++ APBD_cw10_git_s33338/APBD_cw10_git_s33338/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using APBD_cw10_git_s33338.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBD_cw10_git_s33338.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<PC> PCs { get; set; }
+    public DbSet<PCComponent> PCComponents { get; set; }
+    public DbSet<Component> Components { get; set; }
+    public DbSet<ComponentType> ComponentTypes { get; set; }
+    public DbSet<ComponentManufacturer> ComponentManufacturers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
